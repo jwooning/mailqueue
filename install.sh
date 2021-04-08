b053e7044a9dbdcf2fd841763bfe59451a0927e3
Joost Wooning <jwooning@gmail.com>
2021-04-08 22:12:11 +0200

    Working systemd service

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
 pipenv install
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-cp mailqueue.service /etc/systemd/system/mailqueue.service
-chown root:root /etc/systemd/system/mailqueue.service
+cp mailqueue.service .mailqueue.service
 
-sed -i "s#<WORKING_DIR>#"$SCRIPTPATH"#g" /etc/systemd/system/mailqueue.service
-sed -i "s#<PIPENV_PATH>#"$(which pipenv)"#g" /etc/systemd/system/mailqueue.service
+sed -i "s#<USER>#"$(id -u -n)"#g" .mailqueue.service
+sed -i "s#<GROUP>#"$(id -g -n)"#g" .mailqueue.service
+sed -i "s#<WORKING_DIR>#"$SCRIPTPATH"#g" .mailqueue.service
+sed -i "s#<PIPENV_PATH>#"$(which pipenv)"#g" .mailqueue.service
+
+sudo mv .mailqueue.service /etc/systemd/system/mailqueue.service
+sudo chown root:root /etc/systemd/system/mailqueue.service
 
 echo "Installed, to start run 'systemctl start mailqueue.service'"
